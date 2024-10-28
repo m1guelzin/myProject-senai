de62aa88e8c6387cab5b538a0ea0acedcffee66d
m1guelzin <miguelgarridodev@gmail.com>
2024-10-27 22:42:55 -0300

    Apresentacao sprint/2

--- api/src/controllers/userController.js
+++ api/src/controllers/userController.js
@@ -75,81 +75,64 @@ module.exports = class userController {
     }
   }
 
-  static async getAllUsers(req, res) {
-    const queryGet = `SELECT * FROM usuario`;
-    try {
-      connect.query(queryGet, function (err, results) {
-        if (err) {
-          console.error(err);
-          return req.status(500).json({ error: "Erro interno do Servidor" });
-        }
-        return res
-          .status(200)
-          .json({ message: "Lista de Usuários", users: results });
-      });
-    }
-    catch (error) {
-      console.error("Erro ao executar consulta:", error)
-      return res.status(500).json({ error: "Erro interno do Servidor" })
-    }
-  }
 
-  static async updateUser(req, res) {
-    // Desestrutura e recupera os dados enviados via corpo da requisição
-    const { cpf, nome, telefone, email, senha } = req.body;
 
-    // Validar se todos os campos foram preenchidos
-    if (!cpf || !nome || !telefone || !email || !senha) {
-      return res
-        .status(400)
-        .json({ error: "Todos os campos devem ser preenchidos" });
-    }
-    const queryUpdate = `UPDATE usuario SET cpf=?, nome=?, telefone=?, email=?, senha=? WHERE id_usuario = ?`;
-    const values = [cpf, nome, telefone, email, senha];
+  // static async updateUser(req, res) {
+  //   // Desestrutura e recupera os dados enviados via corpo da requisição
+  //   const { cpf, nome, telefone, email, senha } = req.body;
 
-    try {
-      connect.query(queryUpdate, values, function (err, results) {
-        if (err) {
-          if (err.code === "ER_DUP_ENTRY") {
-            return res.status(400).json({ error: "Email já cadastrado por outro usuário" });
-          } else {
-            console.error(err);
-            res.status(500).json({ error: "Erro interno do Servidor" })
-          }
-        }
-        if (results.affectedRows === 0) {
-          return res.status(404).json({ message: "Usuário não encontrado" });
-        }
-        return res.status(200).json({ message: "Usuário atualizado com sucesso" });
-      });
-    } catch (error) {
-      console.error("Erro ao executar consulta", error);
-      return res.status(500).json({ error: "Erro interno do Servidor" });
-    }
-  }
+  //   // Validar se todos os campos foram preenchidos
+  //   if (!cpf || !nome || !telefone || !email || !senha) {
+  //     return res
+  //       .status(400)
+  //       .json({ error: "Todos os campos devem ser preenchidos" });
+  //   }
+  //   const queryUpdate = `UPDATE usuario SET cpf=?, nome=?, telefone=?, email=?, senha=? WHERE id_usuario = ?`;
+  //   const values = [cpf, nome, telefone, email, senha];
 
-  static async deleteUser(req, res) {
-    // Obtem o parametro 'id' da requisição, que é o cpf do user a ser deletado
-    const userId = req.params.id;
-    const queryDelete = `DELETE FROM usuario WHERE id_usuario=?`;
-    const values = [userId];
+  //   try {
+  //     connect.query(queryUpdate, values, function (err, results) {
+  //       if (err) {
+  //         if (err.code === "ER_DUP_ENTRY") {
+  //           return res.status(400).json({ error: "Email já cadastrado por outro usuário" });
+  //         } else {
+  //           console.error(err);
+  //           res.status(500).json({ error: "Erro interno do Servidor" })
+  //         }
+  //       }
+  //       if (results.affectedRows === 0) {
+  //         return res.status(404).json({ message: "Usuário não encontrado" });
+  //       }
+  //       return res.status(200).json({ message: "Usuário atualizado com sucesso" });
+  //     });
+  //   } catch (error) {
+  //     console.error("Erro ao executar consulta", error);
+  //     return res.status(500).json({ error: "Erro interno do Servidor" });
+  //   }
+  // }
 
-    try {
-      connect.query(queryDelete, values, function (err, results) {
-        if (err) {
-          console.error(err);
-          return res.status(500).json({ error: "Erro interno no servidor" });
-        }
-        if (results.affectedRows === 0) {
-          return res.status(404).json({ error: "Usuário não encontrado" });
-        }
-        return res.status(200).json({ message: "Usuário excluido com sucesso" });
-      });
-    } catch (error) {
-      console.error(err);
-      return res.status(500).json({ error: "Erro interno do servidor" });
-    }
-  }
+  // static async deleteUser(req, res) {
+  //   // Obtem o parametro 'id' da requisição, que é o cpf do user a ser deletado
+  //   const userId = req.params.id;
+  //   const queryDelete = `DELETE FROM usuario WHERE id_usuario=?`;
+  //   const values = [userId];
+
+  //   try {
+  //     connect.query(queryDelete, values, function (err, results) {
+  //       if (err) {
+  //         console.error(err);
+  //         return res.status(500).json({ error: "Erro interno no servidor" });
+  //       }
+  //       if (results.affectedRows === 0) {
+  //         return res.status(404).json({ error: "Usuário não encontrado" });
+  //       }
+  //       return res.status(200).json({ message: "Usuário excluido com sucesso" });
+  //     });
+  //   } catch (error) {
+  //     console.error(err);
+  //     return res.status(500).json({ error: "Erro interno do servidor" });
+  //   }
+  // }
   
   //Inicio das condição de Login ---------------------------------------
   static async loginUser(req, res) {
--- api/src/routes/apiRoutes.js
+++ api/src/routes/apiRoutes.js
@@ -3,14 +3,12 @@
 const router = require('express').Router();
 
 // Importa a controller onde contém a lógica relacionada a professores
-const { route } = require('..');
 const userController = require("../controllers/userController")
 
 
 router.post('/user', userController.createUser);
-router.get('/user', userController.getAllUsers);
-router.put('/user', userController.updateUser);
-router.delete('/user/:id', userController.deleteUser);
+//router.put('/user', userController.updateUser);
+//router.delete('/user/:id', userController.deleteUser);
 router.post('/login', userController.loginUser);
 
 module.exports = router
